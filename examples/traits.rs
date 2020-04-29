fa451d879ffa1964328f700a463653a4febb810a
Ed Cragg <drq.11235@gmail.com>
2020-04-29 10:58:17 +0100

    Simplify trait example

--- examples/traits.rs
+++ examples/traits.rs
@@ -5,21 +5,9 @@ pub trait Plugin {
 struct MyPlugin {}
 struct MyOtherPlugin {}
 
-impl MyPlugin {
-    fn new() -> Self {
-        Self {}
-    }
-}
-
 impl Plugin for MyPlugin {
     fn go(&self) -> bool {
         true
-    }
-}
-
-impl MyOtherPlugin {
-    fn new() -> Self {
-        Self {}
     }
 }
 
@@ -30,10 +18,12 @@ impl Plugin for MyOtherPlugin {
 }
 
 fn main() {
-    let my_plugin = MyPlugin::new();            // instantiate a plugin
-    let result = my_plugin.go();                // call the trait method
-    let my_other_plugin = MyOtherPlugin::new(); // instantiate a plugin
-    let result2 = my_other_plugin.go();         // call the trait method
+    let my_plugin = MyPlugin {};            // instantiate a plugin
+    let my_other_plugin = MyOtherPlugin {}; // instantiate a plugin
+
+    let result = my_plugin.go();            // call the trait method
+    let result2 = my_other_plugin.go();     // call the trait method
+
     println!("{}, {}", result, result2);
 }
 
