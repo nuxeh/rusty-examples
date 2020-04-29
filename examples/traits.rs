966a91907e511d479af5a84d05bcd1beb3aadea9
Ed Cragg <drq.11235@gmail.com>
2020-04-29 09:40:13 +0100

    Trait example

--- examples/traits.rs
+++ examples/traits.rs
@@ -1,4 +1,39 @@
-fn main() {
+pub trait Plugin {
+    fn go(&self) -> bool;
+}
 
+struct MyPlugin {}
+struct MyOtherPlugin {}
+
+impl MyPlugin {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Plugin for MyPlugin {
+    fn go(&self) -> bool {
+        true
+    }
+}
+
+impl MyOtherPlugin {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Plugin for MyOtherPlugin {
+    fn go(&self) -> bool {
+        false
+    }
+}
+
+fn main() {
+    let my_plugin = MyPlugin::new();            // instantiate a plugin
+    let result = my_plugin.go();                // call the trait method
+    let my_other_plugin = MyOtherPlugin::new(); // instantiate a plugin
+    let result2 = my_other_plugin.go();         // call the trait method
+    println!("{}, {}", result, result2);
 }
 
