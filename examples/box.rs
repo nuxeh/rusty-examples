7d05c6abba8f84c310e823d671aab2ff0347e84a
Ed Cragg <drq.11235@gmail.com>
2020-04-29 12:31:22 +0100

    Add box

---
+++ examples/box.rs
@@ -0,0 +1,21 @@
+#[allow(dead_code)]
+struct Nested {
+//    nested: Nested,
+}
+
+#[allow(dead_code)]
+struct NestedBox {
+    nested_box: Option<Box<NestedBox>>,
+}
+
+fn main() {
+    let foo = NestedBox { nested_box: None };
+    let _bar = NestedBox { nested_box: Some(Box::new(foo)) };
+
+    // But what happens when you try to clone a Box?
+    let baz = Box::new(1);
+    let qux = baz.clone();
+    // It's ok, but you will have duplicated the memory for the contents.
+}
+
+// https://doc.rust-lang.org/book/ch15-01-box.html
